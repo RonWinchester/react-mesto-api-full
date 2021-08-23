99f4777ee7efad71ebae5f20126ec26e53b86f2b
Ron Winchester <RonWinchester@yandex.ru>
2021-08-23 21:51:09 +0300

    auth

--- backend/middlewares/auth.js
+++ backend/middlewares/auth.js
@@ -1,4 +1,6 @@
 const jwt = require('jsonwebtoken');
+
+const { NODE_ENV, JWT_SECRET } = process.env;
 const AuthorizationError = require('../errors/AuthorizationError');
 
 module.exports = (req, res, next) => {
@@ -8,7 +10,7 @@ module.exports = (req, res, next) => {
   const token = req.cookies.jwt;
   let payload;
   try {
-    payload = jwt.verify(token, process.env.JWT_SECRET);
+    payload = jwt.verify(token, NODE_ENV === 'production' ? JWT_SECRET : 'dev-secret');
   } catch (err) {
     throw new AuthorizationError('Необходима Авторизация');
   }
