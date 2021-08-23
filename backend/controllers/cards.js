db9408ad8c360178c29158d79e5e2f83ab507a40
Ron Winchester <RonWinchester@yandex.ru>
2021-08-24 02:04:32 +0300

    data

--- backend/controllers/cards.js
+++ backend/controllers/cards.js
@@ -18,7 +18,7 @@ module.exports.createCard = (req, res, next) => {
   const owner = req.user._id;
   console.log(owner);
   Card.create({ name, link, owner })
-    .then((card) => res.status(200).send(card))
+    .then((user) => res.status(200).send(user.card))
     .catch((err) => {
       if (err.name === 'ValidationError') {
         const error = new BadRequest('Переданы некорректные данные');
