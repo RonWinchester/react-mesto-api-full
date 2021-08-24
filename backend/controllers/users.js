b6bdf776983e7181383786319189ace15113cdb4
Ron Winchester <RonWinchester@yandex.ru>
2021-08-24 21:07:01 +0300

    maxAge cookie

--- backend/controllers/users.js
+++ backend/controllers/users.js
@@ -19,6 +19,7 @@ module.exports.login = (req, res, next) => {
         httpOnly: true,
         sameSite: 'None',
         secure: true,
+        expires: new Date(Date.now() + (60 * 24 * 3600000)),
       })
         .status(200).send({ token });
     })
